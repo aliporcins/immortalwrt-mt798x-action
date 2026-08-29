7a9cb6e3ec67f2201f213cd9371af549ef32da28
hench <38806400+aliporcins@users.noreply.github.com>
2026-08-29 17:54:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,9 +18,3 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # 添加 daed 源码
 git clone https://github.com/kenzok8/openwrt-daede.git package/openwrt-daede
-# ========== 修复 OpenClash 的 GEOIP 数据下载源 ==========
-# 将 raw.githubusercontent.com 替换为 ghproxy.net 代理
-sed -i 's|raw.githubusercontent.com|ghproxy.net/https://raw.githubusercontent.com|g' feeds/luci/applications/luci-app-openclash/root/usr/share/openclash/geoip_dat_update.sh
-
-# 将 github.com 的下载链接也替换为代理
-sed -i 's|https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat|https://ghproxy.net/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat|g' feeds/luci/applications/luci-app-openclash/root/usr/share/openclash/geoip_dat_update.sh
