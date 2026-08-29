a59c9a309e4c5db31a0d299c74b914777721ef13
hench <38806400+aliporcins@users.noreply.github.com>
2026-08-29 18:15:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
 # ========== 添加 MTK 闭源驱动 ==========
-# 从 hanwckf 仓库提取 MTK 驱动相关包
-git clone --depth=1 -b openwrt-21.02 https://github.com/hanwckf/immortalwrt-mt798x package/hanwckf-mt798x
+# 使用 237 的驱动仓库（专门为 MT7981 优化）
+git clone https://github.com/immortalwrt-mt798x/immortalwrt-mt798x package/mtk-driver
 
-# 只保留必要的驱动目录（如果克隆了整个仓库，需要复制驱动到正确位置）
-# 或者使用更精确的驱动源：
-# git clone https://github.com/hanwckf/mt798x-wifi-pkg package/mtk-wifi
+# 或者使用 1715173329 的驱动
+# git clone https://github.com/1715173329/immortalwrt-mt798x package/mtk-driver
 
 # ========== 添加 daed 源码 ==========
 git clone https://github.com/kenzok8/openwrt-daede.git package/openwrt-daede
