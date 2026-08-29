cec6727586dac6cb52b35ea00964296cd8f36e3e
hench <38806400+aliporcins@users.noreply.github.com>
2026-08-29 13:24:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 添加 daed 源码
+git clone https://github.com/kenzok8/openwrt-daede.git package/openwrt-daede
