938bac6d700af13b55ad840979ad8230097531a1
hench <38806400+aliporcins@users.noreply.github.com>
2026-08-30 11:47:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
 # ========== 添加 MTK 闭源驱动 (MT7981) ==========
 # 使用 padavanonly 的驱动仓库（支持 6.x 内核）
